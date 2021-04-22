709369deb83f594ff35fa08b2d02ba4e4854587d
Florian <florian.gacon@ifsttar.fr>
2021-04-22 16:53:18 +0200

    segmm_ problem

--- recipe/build-dolfinx.sh
+++ recipe/build-dolfinx.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd dolfinx
-
-cd cpp
-mkdir build
-cd build
-
 export LIBRARY_PATH=$PREFIX/lib
 export INCLUDE_PATH=$PREFIX/include
 
@@ -13,17 +7,26 @@ export PETSC_DIR=$PREFIX
 export SLEPC_DIR=$PREFIX
 export BLAS_DIR=$LIBRARY_PATH
 
+cd dolfinx
+cd cpp
+mkdir build
+cd build
+
 cmake .. \
-  -DDOLFIN_ENABLE_MPI=on \
-  -DDOLFIN_ENABLE_PETSC=on \
-  -DDOLFIN_ENABLE_SLEPC=on \
-  -DDOLFIN_ENABLE_SCOTCH=on \
-  -DDOLFIN_ENABLE_HDF5=on \
+  -DDOLFINX_ENABLE_MPI=on \
+  -DDOLFINX_ENABLE_PETSC=on \
+  -DDOLFINX_ENABLE_SLEPC=on \
+  -DDOLFINX_ENABLE_SCOTCH=on \
+  -DDOLFINX_ENABLE_HDF5=on \
+  -DDOLFINX_SKIP_BUILD_TESTS=FALSE \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
   -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
   -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH \
-  -DPython3_ROOT_DIR=$PREFIX || (cat CMakeFiles/CMakeError.log && exit 1)
+  -DPython3_ROOT_DIR=$PREFIX \
+  -DPython3_FIND_STRATEGY=LOCATION \
+  -DSCOTCH_ROOT=$PREFIX \
+  -DHDF5_ROOT=$PREFIX || (cat CMakeFiles/CMakeError.log && exit 1)
 make -j$CPU_COUNT install
 
 #
